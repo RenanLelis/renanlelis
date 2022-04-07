a9f3f25cb57d82f816929294ccea9f2486459a68
Renan Lelis <renan.rufino@serpro.gov.br>
2022-04-06 21:36:58 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,18 +1,3 @@
-// Sticky Navigation Menu JS Code
-let nav = document.querySelector("nav");
-let scrollBtn = document.querySelector(".scroll-button a");
-console.log(scrollBtn);
-let val;
-window.onscroll = function () {
-    if (document.documentElement.scrollTop > 20) {
-        nav.classList.add("sticky");
-        scrollBtn.style.display = "block";
-    } else {
-        nav.classList.remove("sticky");
-        scrollBtn.style.display = "none";
-    }
-
-}
 
 // Side NavIgation Menu JS Code
 let body = document.querySelector("body");
